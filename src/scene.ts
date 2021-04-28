c3bae93cfed4fe04e1a4830ed8e010f6df5bd25f
Groctel <groctel@protonmail.com>
2021-04-28 10:53:37 +0200

    Added default game scene

---
+++ src/scene.ts
@@ -0,0 +1,140 @@
+import * as $ from 'jquery';
+import * as THREE from 'three';
+import { GUI } from 'dat-gui';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+
+const SCENE_DEFAULTS = {
+	AXES:            true,
+	LIGHT_INTENSITY: 0.5,
+};
+
+class GameScene extends THREE.Scene
+{
+	axes: THREE.AxesHelper;
+	properties: {
+		axes: boolean,
+		light_intensity: number,
+		default: VoidFunction
+	};
+	gui: GUI;
+	renderer: THREE.WebGLRenderer;
+	camera: THREE.PerspectiveCamera;
+	spotlight: THREE.SpotLight;
+	camera_control: TrackballControls;
+
+	constructor (canvas: string)
+	{
+		super();
+
+		this.properties = {
+			axes:            SCENE_DEFAULTS.AXES,
+			light_intensity: SCENE_DEFAULTS.LIGHT_INTENSITY,
+
+			default: () =>
+			{
+				this.properties.axes            = SCENE_DEFAULTS.AXES;
+				this.properties.light_intensity = SCENE_DEFAULTS.LIGHT_INTENSITY;
+			}
+		};
+
+		this.renderer = this.constructRenderer(canvas);
+		this.gui      = this.constructGUI();
+		this.constructLights();
+		this.constructCamera();
+
+		this.axes = new THREE.AxesHelper (50);
+		this.add(this.axes);
+	}
+
+	constructCamera (): void
+	{
+		this.camera = new THREE.PerspectiveCamera(
+			45,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000
+		);
+
+		const look = new THREE.Vector3(0, 0, 0);
+
+		this.camera.position.set(20, 10, 10);
+		this.camera.lookAt(look);
+		this.add(this.camera);
+
+		this.camera_control = new TrackballControls(
+			this.camera,
+			this.renderer.domElement
+		);
+
+		this.camera_control.rotateSpeed = 5;
+		this.camera_control.zoomSpeed   = 2;
+		this.camera_control.panSpeed    = 0.5;
+		this.camera_control.target      = look;
+	}
+
+	constructGUI (): GUI
+	{
+		const gui    = new GUI();
+		const folder = gui.addFolder("Lights and axes");
+
+		folder
+			.add(this.properties, 'light_intensity', 0, 1)
+			.name("Light intensity");
+
+		folder
+			.add(this.properties, 'axes')
+			.name("Show axes");
+
+		return gui;
+	}
+
+	constructLights (): void
+	{
+		const light    = new THREE.AmbientLight(0xccddee, 0.35);
+		this.spotlight = new THREE.SpotLight(
+			0xffffff,
+			this.properties.light_intensity
+		);
+
+		this.spotlight.position.set(60, 60, 40);
+
+		this.add(light);
+		this.add(this.spotlight);
+	}
+
+	constructRenderer (canvas: string): THREE.WebGLRenderer
+	{
+		const renderer = new THREE.WebGLRenderer();
+
+		renderer.setClearColor(new THREE.Color(0xEEEEEE), 1.0);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		$(canvas).append(renderer.domElement);
+
+		return renderer;
+	}
+
+	setCameraAspect (ratio: number): void
+	{
+		this.camera.aspect = ratio;
+		this.camera.updateProjectionMatrix();
+	}
+
+	onWindowResize (): void
+	{
+		this.setCameraAspect(window.innerWidth / window.innerHeight);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	update (): void
+	{
+		this.renderer.render(this, this.camera);
+		this.spotlight.intensity = this.properties.light_intensity;
+		this.camera_control.update();
+
+		this.axes.visible = this.properties.axes;
+
+		requestAnimationFrame(() => this.update());
+	}
+}
+
+export { GameScene };
